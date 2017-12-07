13bbd8f87d15cc95259ffdaa1152c7e4ca71a418
julio olivares <julioolivares90@gmail.com>
2017-12-06 19:14:26 -0600

    metodos del controlador

--- src/java/controlador/ProductoControl.java
+++ src/java/controlador/ProductoControl.java
@@ -8,6 +8,7 @@ package controlador;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
+import modelo.dao.ProductoDao;
 import modelo.entidad.Producto;
 
 /**
@@ -24,7 +25,31 @@ public class ProductoControl {
      * Creates a new instance of ProductoControl
      */
     public ProductoControl() {
+        producto = new Producto();
     }
-    
-    
+    public List<Producto>getListaProducto(){
+        ProductoDao p = new ProductoDao();
+        listaProducto = p.listaProducto();
+        return listaProducto;
+    }
+    public void setListaProducto(List<Producto>listaProducto){
+        this.listaProducto = listaProducto;
+    }
+    public void setProducto(Producto producto){
+        this.producto=producto;
+    }
+    public Producto getProducto(){
+        return this.producto;
+    }
+    public void limpiarProducto(){
+        producto = new Producto();
+    }
+    public void agregarProducto(){
+        ProductoDao p = new ProductoDao();
+        p.agregar(producto);
+    }
+    public void modificarProducto(){
+        ProductoDao p = new ProductoDao();
+        p.actualizar(producto);
+    }
 }
