68d2bb713605363e36e981bd58b17af329015fc3
julio olivares <julioolivares90@gmail.com>
2017-12-07 16:12:53 -0600

    producto control

---
+++ src/java/controlador/ProductoControl.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.inject.Named;
+import javax.faces.bean.ViewScoped;
+import modelo.dao.ProductoDao;
+import modelo.entidad.Producto;
+
+/**
+ *
+ * @author julio
+ */
+@ManagedBean
+@ViewScoped
+public class ProductoControl {
+
+    /**
+     * Creates a new instance of ProductoControl
+     */
+    private List<Producto> listaProducto;
+    private Producto producto;
+    public ProductoControl() {
+       producto = new Producto();
+    }
+    public List<Producto>getListaProducto(){
+        ProductoDao p = new ProductoDao();
+        listaProducto = p.listaProducto();
+        return listaProducto;
+    }
+     public void setListaProducto(List<Producto>listaProducto){
+        this.listaProducto = listaProducto;
+    }
+    public void setProducto(Producto producto){
+        this.producto=producto;
+    }
+    public Producto getProducto(){
+        return this.producto;
+    }
+    public void limpiarProducto(){
+        producto = new Producto();
+    }
+    public void agregarProducto(){
+        ProductoDao p = new ProductoDao();
+        p.agregar(producto);
+    }
+    public void modificarProducto(){
+        ProductoDao p = new ProductoDao();
+        p.actualizar(producto);
+    }
+    
+    
+}
