f1eb24bf53a43c2efd1096842537a36dda900dad
carlos <carlos@169.254.174.129>
2017-12-08 15:06:19 -0600

    Agrege FacturaDao

---
+++ src/java/modelo/dao/FacturaDao.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.dao;
+
+import java.util.List;
+import modelo.entidad.Factura;
+import modelo.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Mcortez
+ */
+public class FacturaDao {
+    
+     public List<Factura> listarFactura() {
+        List<Factura> lista = null;
+        Session sesion = HibernateUtil.getSessionFactory().openSession();
+        Transaction t = sesion.beginTransaction();
+        String hql = "FROM Factura";
+        try {
+            lista = sesion.createQuery(hql).list();
+            t.commit();
+            sesion.close();
+        } catch (Exception e) {
+            t.rollback();
+        }
+        return lista;
+    }
+    
+      public void agregar(Factura bodegas) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.save(bodegas);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
+
+    public void modificar(Factura bodegas) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.update(bodegas);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
+
+    public void eliminar(Factura bodegas) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.delete(bodegas);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
+    
+}
